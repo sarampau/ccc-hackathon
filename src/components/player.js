455a34a953bc97ff1518a9bf5116071cdfb1f9a6
Manny <mannysth@yahoo.com>
2022-05-21 14:04:34 -0700

    finishes styling player component and clears edge cases, ready for inpection

--- src/components/player.js
+++ src/components/player.js
@@ -6,7 +6,7 @@ const Player = (props) => {
   const number = props.number;
   return (
     <div className='keeper-container'>
-      <div>
+      <div className='keeper-header'>
         <p
           className='return-button'
           onClick={ () => props.handleReturn() }
@@ -14,17 +14,14 @@ const Player = (props) => {
           Back to Team
         </p>
         <img src={stats.team.logo}/>
-        <h3>{stats.team.name}</h3>
+        <p>{stats.team.name}</p>
       </div>
       {
         stats.games.position === 'Goalkeeper' ?
           <div className='player-row'>
             <div className='info'>
-              <div>
-                <img src={info.photo}/>
-                <p>{info.firstname}</p>
-                <p>{info.lastname}</p>
-              </div>
+              <img src={info.photo}/>
+              <p>{info.firstname} {info.lastname}</p>
               <p>Number: {number}</p>
               <p>Age: {info.age}</p>
               <p>Nationality: {info.nationality}</p>
@@ -33,19 +30,15 @@ const Player = (props) => {
               <p>Appearences: {stats.games.appearences}</p>
               <p>Goals Saved: {stats.goals.saves}</p>
               <p>Goals Conceded: {stats.goals.conceded}</p>
-              <p>Cards:</p>
-              <p className='cards'>Yellow: {stats.cards.yellow}</p>
-              <p className='cards'>Red: {stats.cards.red}</p>
+              <p><span style={{color: '#aaaa38'}}>Yellow Cards: </span>{stats.cards.yellow}</p>
+              <p><span style={{color: 'red'}}>Red Cards: </span>{stats.cards.red}</p>
             </div>
           </div>
         :
           <div className='player-row'>
             <div className='info'>
-              <div>
-                <img src={info.photo}/>
-                <p>{info.firstname}</p>
-                <p>{info.lastname}</p>
-              </div>
+              <img src={info.photo}/>
+              <p>{info.firstname} {info.lastname}</p>
               <p>Number: {number}</p>
               <p>Age: {info.age}</p>
               <p>Nationality: {info.nationality}</p>
@@ -55,9 +48,8 @@ const Player = (props) => {
               <p>Goals: {stats.goals.total}</p>
               <p>Assists: {stats.goals.assists}</p>
               <p>Pass Accuracy: {stats.passes.accuracy}%</p>
-              <p>Cards:</p>
-              <p className='cards'>Yellow: {stats.cards.yellow}</p>
-              <p calssName='cards'>Red: {stats.cards.red}</p>
+              <p><span style={{color: '#aaaa38'}}>Yellow Cards: </span>{stats.cards.yellow}</p>
+              <p><span style={{color: 'red'}}>Red Cards: </span>{stats.cards.red}</p>
             </div>
           </div>
       }
